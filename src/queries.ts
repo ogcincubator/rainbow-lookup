fe6d32cc7af62ac3830a1f3bbffeb0d53a0513ef
Alejandro Villar <avillar@ogc.org>
2025-04-09 17:12:41 +0200

    Ensure resources are only retrieved once

--- src/queries.ts
+++ src/queries.ts
@@ -1,12 +1,14 @@
 const queries: Record<string, string> = {
     FETCH_CONCEPT_SCHEMES: `
         PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-        SELECT DISTINCT ?cs ?label WHERE { ?cs a skos:ConceptScheme ; skos:prefLabel ?label }
+        SELECT DISTINCT ?cs (MIN(?lbl) as ?label) WHERE { ?cs a skos:ConceptScheme ; skos:prefLabel ?lbl }
+        GROUP BY ?cs
         ORDER BY ?label
     `,
     FETCH_CONCEPT_SCHEME_CONCEPTS: `
         PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-        SELECT DISTINCT ?c ?label WHERE { ?c a skos:Concept ; skos:prefLabel ?label ; skos:inScheme <@@CS_URI@@> }
+        SELECT DISTINCT ?c (MIN(?lbl) as ?label) WHERE { ?c a skos:Concept ; skos:prefLabel ?lbl ; skos:inScheme <@@CS_URI@@> }
+        GROUP BY ?c
         ORDER BY ?label
     `,
 }
