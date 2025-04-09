0ce0e8cc06c03c151d3d13cfa00c0ccda2decf55
Alejandro Villar <avillar@ogc.org>
2025-04-09 16:28:24 +0200

    Add error levels and callbacks

--- src/main.ts
+++ src/main.ts
@@ -4,21 +4,27 @@ import autocomplete from "@tarekraafat/autocomplete.js";
 import './style.css';
 
 export type AutocompleteOptions = {
-    onConceptSchemeSelected?: (conceptScheme: ConceptScheme | null) => void,
-    onConceptSelected?: (concept: SKOSResource | null) => void,
-    onConceptSchemeLoaded?: (conceptScheme: ConceptScheme, concepts: SKOSResource[]) => void,
+    onConceptSchemesLoaded?: (conceptSchemes: ConceptScheme[]) => void;
+    onConceptSchemeSelected?: (conceptScheme: ConceptScheme | null) => void;
+    onConceptSelected?: (concept: SKOSResource | null) => void;
+    onConceptSchemeLoaded?: (conceptScheme: ConceptScheme, concepts: SKOSResource[]) => void;
+    onError?: (action: string, error: any) => void;
     loading?: {
-        conceptSchemes?: string,
-        concepts?: string,
-    },
+        conceptSchemes?: string;
+        concepts?: string;
+    };
     noResults?: {
-        conceptSchemes?: string,
-        concepts?: string,
-    },
+        conceptSchemes?: string;
+        concepts?: string;
+    };
     placeholder?: {
-        conceptScheme?: string,
-        concept?: string,
-    },
+        conceptScheme?: string;
+        concept?: string;
+    };
+    errorMessages: {
+        conceptSchemes?: string;
+        concepts?: string;
+    };
 };
 
 export type AutocompleteInstance = {
@@ -27,139 +33,163 @@ export type AutocompleteInstance = {
 
 export function create(element: HTMLElement, sparqlEndpoint: string, options?: AutocompleteOptions): AutocompleteInstance {
     const noConceptSchemesFoundLabel = document.createElement("div");
-    noConceptSchemesFoundLabel.style.display = "none";
+    noConceptSchemesFoundLabel.className = 'no-results no-results-concept-schemes';
     noConceptSchemesFoundLabel.innerHTML = options?.noResults?.concepts || 'No concept schemes found';
+
+    const conceptSchemesErrorLabel = document.createElement("div");
+    conceptSchemesErrorLabel.className = 'error error-concept-schemes';
+    conceptSchemesErrorLabel.innerHTML = options?.errorMessages?.conceptSchemes || 'Error retrieving concept schemes';
 
     const conceptSchemeInput = document.createElement("input");
     const conceptInput = document.createElement("input");
     const noConceptsFoundLabel = document.createElement("div");
     const loadingConceptSchemesLabel = document.createElement("div");
     const loadingConceptsLabel = document.createElement("div");
+    const conceptsErrorLabel = document.createElement("div");
 
     let conceptScheme: ConceptScheme | null = null;
     let concepts: SKOSResource[] | null = null;
 
     conceptSchemeInput.className = 'concept-scheme-input';
     conceptInput.className = 'concept-input';
-    noConceptsFoundLabel.className = 'no-concepts-found';
+    noConceptsFoundLabel.className = 'no-results no-results-concepts';
     loadingConceptSchemesLabel.className = 'loading loading-concept-schemes';
     loadingConceptSchemesLabel.innerHTML = options?.loading?.conceptSchemes || 'Loading...';
     element.append(loadingConceptSchemesLabel);
     loadingConceptsLabel.style.display = "none";
     loadingConceptsLabel.className = 'loading loading-concepts';
     loadingConceptsLabel.innerHTML = options?.loading?.concepts || 'Loading concepts...';
+    conceptsErrorLabel.style.display = "none";
+    conceptsErrorLabel.className = 'error error-concepts';
+    conceptsErrorLabel.innerHTML = options?.errorMessages?.concepts || 'Error retrieving concepts';
 
-    loadConceptSchemes(sparqlEndpoint).then((conceptSchemes) => {
+    loadConceptSchemes(sparqlEndpoint)
+        .then((conceptSchemes) => {
 
-        element.removeChild(loadingConceptSchemesLabel);
-        if (!conceptSchemes.length) {
-            element.append(noConceptSchemesFoundLabel);
-            return {
-                reset: () => null,
-            };
-        }
+            options?.onConceptSchemesLoaded?.(conceptSchemes);
 
-        noConceptsFoundLabel.style.display = "none";
-        noConceptsFoundLabel.innerHTML = options?.noResults?.concepts || 'No concepts found';
-        conceptInput.style.display = "none";
-        element.append(conceptSchemeInput);
-        element.append(noConceptsFoundLabel);
-        element.append(loadingConceptsLabel);
-        element.append(conceptInput);
-
-        const query = (q: string) => q.toLowerCase();
-        const searchEngine = (query: string, record: SKOSResource) => {
-            if (record.label.toLowerCase().includes(query)) {
-                return record.label;
+            if (!conceptSchemes.length) {
+                element.append(noConceptSchemesFoundLabel);
+                return;
             }
-        };
 
-        const conceptSchemeAutocomplete: any = new autocomplete({
-            name: 'rainbow-concept-schemes',
-            selector: () => conceptSchemeInput,
-            placeHolder: 'Concept scheme',
-            threshold: 0,
-            data: {
-                src: () => {
-                    return conceptSchemes;
-                },
-            },
-            query,
-            searchEngine,
-            resultsList: {
-                'class': 'rainbow-autocomplete-results concept-schemes',
-                maxResults: undefined,
-            },
-            resultItem: {
-                'class': 'rainbow-autocomplete-result concept-scheme',
-            },
-            events: {
-                input: {
-                    focus() {
-                        conceptSchemeAutocomplete.start();
+            noConceptsFoundLabel.style.display = "none";
+            noConceptsFoundLabel.innerHTML = options?.noResults?.concepts || 'No concepts found';
+            conceptInput.style.display = "none";
+            element.append(conceptSchemeInput);
+            element.append(noConceptsFoundLabel);
+            element.append(loadingConceptsLabel);
+            element.append(conceptsErrorLabel);
+            element.append(conceptInput);
+
+            const query = (q: string) => q.toLowerCase();
+            const searchEngine = (query: string, record: SKOSResource) => {
+                if (record.label.toLowerCase().includes(query)) {
+                    return record.label;
+                }
+            };
+
+            const conceptSchemeAutocomplete: any = new autocomplete({
+                name: 'rainbow-concept-schemes',
+                selector: () => conceptSchemeInput,
+                placeHolder: 'Concept scheme',
+                threshold: 0,
+                data: {
+                    src: () => {
+                        return conceptSchemes;
                     },
-                    async selection(ev: any) {
-                        const newConceptScheme = ev.detail.selection.value;
-                        if (conceptScheme && newConceptScheme?.uri === conceptScheme.uri) {
-                            return;
-                        }
-                        conceptScheme = newConceptScheme;
-                        conceptInput.style.display = 'none';
-                        conceptInput.value = '';
-                        noConceptsFoundLabel.style.display = 'none';
-                        if (conceptScheme != null) {
-                            conceptSchemeInput.value = conceptScheme.label;
-                            options?.onConceptSchemeSelected?.(conceptScheme);
-                            loadingConceptsLabel.style.display = "";
-                            concepts = await loadConcepts(sparqlEndpoint, conceptScheme!.uri);
-                            loadingConceptsLabel.style.display = "none";
-                            options?.onConceptSchemeLoaded?.(conceptScheme, concepts);
-                            if (concepts.length) {
-                                conceptInput.style.display = '';
-                                conceptInput.focus();
-                                conceptAutocomplete.start();
-                            } else {
-                                noConceptsFoundLabel.style.display = '';
+                },
+                query,
+                searchEngine,
+                resultsList: {
+                    'class': 'rainbow-autocomplete-results concept-schemes',
+                    maxResults: undefined,
+                },
+                resultItem: {
+                    'class': 'rainbow-autocomplete-result concept-scheme',
+                },
+                events: {
+                    input: {
+                        focus() {
+                            conceptSchemeAutocomplete.start();
+                        },
+                        async selection(ev: any) {
+                            const newConceptScheme = ev.detail.selection.value;
+                            if (conceptScheme && newConceptScheme?.uri === conceptScheme.uri) {
+                                return;
+                            }
+                            conceptScheme = newConceptScheme;
+                            conceptInput.style.display = 'none';
+                            conceptInput.value = '';
+                            noConceptsFoundLabel.style.display = 'none';
+                            conceptsErrorLabel.style.display = 'none';
+                            if (conceptScheme != null) {
+                                conceptSchemeInput.value = conceptScheme.label;
+                                options?.onConceptSchemeSelected?.(conceptScheme);
+                                loadingConceptsLabel.style.display = "";
+                                try {
+                                    concepts = await loadConcepts(sparqlEndpoint, conceptScheme!.uri);
+
+                                    options?.onConceptSchemeLoaded?.(conceptScheme, concepts);
+                                    if (concepts.length) {
+                                        conceptInput.style.display = '';
+                                        conceptInput.focus();
+                                        conceptAutocomplete.start();
+                                    } else {
+                                        noConceptsFoundLabel.style.display = '';
+                                    }
+                                } catch (err) {
+                                    conceptsErrorLabel.style.display = '';
+                                    options?.onError?.('loadConcepts', err);
+                                } finally {
+                                    loadingConceptsLabel.style.display = "none";
+                                }
                             }
                         }
-                    }
-                },
-            },
-        });
-
-        const conceptAutocomplete: any = new autocomplete({
-            name: 'rainbow-concepts',
-            selector: () => conceptInput,
-            placeHolder: 'Concept',
-            threshold: 0,
-            data: {
-                src: () => {
-                    return concepts || [];
-                },
-            },
-            query,
-            searchEngine,
-            resultsList: {
-                'class': 'rainbow-autocomplete-results concept-schemes',
-                maxResults: undefined,
-            },
-            resultItem: {
-                'class': 'rainbow-autocomplete-result concept-scheme',
-            },
-            events: {
-                input: {
-                    focus() {
-                        conceptAutocomplete.start();
                     },
-                    selection(ev: any) {
-                        const concept = ev.detail.selection.value;
-                        conceptInput.value = concept.label;
-                        options?.onConceptSelected?.(concept);
-                    }
                 },
-            },
+            });
+
+            const conceptAutocomplete: any = new autocomplete({
+                name: 'rainbow-concepts',
+                selector: () => conceptInput,
+                placeHolder: 'Concept',
+                threshold: 0,
+                data: {
+                    src: () => {
+                        return concepts || [];
+                    },
+                },
+                query,
+                searchEngine,
+                resultsList: {
+                    'class': 'rainbow-autocomplete-results concept-schemes',
+                    maxResults: undefined,
+                },
+                resultItem: {
+                    'class': 'rainbow-autocomplete-result concept-scheme',
+                },
+                events: {
+                    input: {
+                        focus() {
+                            conceptAutocomplete.start();
+                        },
+                        selection(ev: any) {
+                            const concept = ev.detail.selection.value;
+                            conceptInput.value = concept.label;
+                            options?.onConceptSelected?.(concept);
+                        }
+                    },
+                },
+            });
+        })
+        .catch(err => {
+            element.append(conceptSchemesErrorLabel);
+            options?.onError?.('loadConceptSchemes', err);
+        })
+        .finally(() => {
+            element.removeChild(loadingConceptSchemesLabel);
         });
-    });
 
     const reset = () => {
         conceptSchemeInput.value = '';
